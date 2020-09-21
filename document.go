089e35eaaff1737a0de3602aef6cc0d6b121c73a
Kotaro Adachi <k33asby@gmail.com>
2020-09-21 18:28:40 +0900

    wip: reflect document

--- document.go
+++ document.go
@@ -1,6 +1,9 @@
 package main
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 type DocumentBuilder struct {
 	Fields []reflect.StructField
@@ -10,16 +13,29 @@ func NewDocumentBuilder() *DocumentBuilder {
 	return &DocumentBuilder{}
 }
 
-// string, int, float, bool フィールドを持つ
-func (b *DocumentBuilder) AddField(n, t string) {
-	switch
-	case
+// TODO: reflectよくわからん
+func (b *DocumentBuilder) AddField(name, typ string) error {
+	var t reflect.Type
+	switch typ {
+	case "string":
+		t = reflect.TypeOf("")
+	case "int":
+		t = reflect.TypeOf(1)
+	case "float":
+		t = reflect.TypeOf(0.1)
+	case "bool":
+		t = reflect.TypeOf(true)
+	default:
+		return fmt.Errorf("error: invalid type")
+	}
 	b.Fields = append(
 		b.Fields,
 		reflect.StructField{
-			Name: n,
+			Name: name,
 			Type: t,
-		})
+		},
+	)
+	return nil
 }
 
 func (b *DocumentBuilder) Build() DocumentType {
@@ -43,4 +59,32 @@ func (d *DocumentType) New() *Document {
 
 type Document struct {
 	Fields reflect.Value
+}
+
+func (d *Document) Get(name string) (interface{}, error) {
+	rv := d.Fields.FieldByName(name)
+	if (rv == reflect.Value{}) {
+		return nil, fmt.Errorf("error: not found given field")
+	}
+	return rv.Interface(), nil
+}
+
+func (d *Document) Set(name string, value interface{}) error {
+	rv := d.Fields.FieldByName(name)
+	if (rv == reflect.Value{}) {
+		return fmt.Errorf("error: not found given field")
+	}
+	switch rv.Type() {
+	case reflect.TypeOf(""):
+		rv.SetString(value.(string))
+	case reflect.TypeOf(1):
+		rv.SetInt(int64(value.(int)))
+	case reflect.TypeOf(0.1):
+		rv.SetFloat(value.(float64))
+	case reflect.TypeOf(true):
+		rv.SetBool(value.(bool))
+	default:
+		return fmt.Errorf("error: don't match any type")
+	}
+	return nil
 }
