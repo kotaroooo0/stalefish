863af112826fb0914728926e235503c3b9e6933c
Kotaro Adachi <k33asby@gmail.com>
2020-11-09 00:12:41 +0900

    wip: indexing for map

--- index.go
+++ index.go
@@ -5,17 +5,11 @@ type FieldInvertedIndex map[string][]int
 
 type Index map[string]FieldInvertedIndex
 
-// add adds documents to the index.
-func (idx Index) Add(docs []Document, analyzer Analyzer) {
+func (idx Index) Indexing(docs []Document, analyzer Analyzer) {
 	for _, doc := range docs {
-		rt := doc.Fields.Type()
-		for i := 0; i < rt.NumField(); i++ {
-			field := rt.Field(i)
-			// kind := field.Type.Kind()                   // 型
-			value := doc.Fields.FieldByName(field.Name) // value は interface です。
-
+		for k, v := range doc.Fields {
 			fieldInvertedIndex := FieldInvertedIndex{}
-			for _, token := range analyzer.Analyze(value.String()) {
+			for _, token := range analyzer.Analyze(v) {
 				ids := fieldInvertedIndex[token]
 				if ids != nil && ids[len(ids)-1] == doc.ID {
 					// Don't add same ID twice.
@@ -23,7 +17,31 @@ func (idx Index) Add(docs []Document, analyzer Analyzer) {
 				}
 				fieldInvertedIndex[token] = append(ids, doc.ID)
 			}
-			idx.FieldToInvertedIndex[field.Name] = fieldInvertedIndex
+			idx[k] = fieldInvertedIndex
 		}
 	}
 }
+
+// NOTE: Goだけでなく任意の言語から使えるようにする場合はこれでよかった(?)
+// add adds documents to the index.
+// func (idx Index) Add(docs []Document, analyzer Analyzer) {
+// 	for _, doc := range docs {
+// 		rt := doc.Fields.Type()
+// 		for i := 0; i < rt.NumField(); i++ {
+// 			field := rt.Field(i)
+// 			// kind := field.Type.Kind()                   // 型
+// 			value := doc.Fields.FieldByName(field.Name) // value は interface です。
+
+// 			fieldInvertedIndex := FieldInvertedIndex{}
+// 			for _, token := range analyzer.Analyze(value.String()) {
+// 				ids := fieldInvertedIndex[token]
+// 				if ids != nil && ids[len(ids)-1] == doc.ID {
+// 					// Don't add same ID twice.
+// 					continue
+// 				}
+// 				fieldInvertedIndex[token] = append(ids, doc.ID)
+// 			}
+// 			idx.FieldToInvertedIndex[field.Name] = fieldInvertedIndex
+// 		}
+// 	}
+// }
