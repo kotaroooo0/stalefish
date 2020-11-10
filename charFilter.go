e233b1b5d5221c281960a5807495785a8378c899
Kotaro Adachi <k33asby@gmail.com>
2020-11-10 23:42:57 +0900

    add mapping char filter

--- charFilter.go
+++ charFilter.go
@@ -1,5 +1,18 @@
 package stalefish
 
+import "strings"
+
 type CharFilter interface {
 	Filter(string) string
+}
+
+type MappingCharFilter struct {
+	Mapper map[string]string // key->valueにマッピングする
+}
+
+func (c MappingCharFilter) Filter(s string) string {
+	for k, v := range c.Mapper {
+		s = strings.Replace(s, k, v, -1)
+	}
+	return s
 }
