82ba371255436714b21b068c60c3408cd4a82569
Kotaro Adachi <k33asby@gmail.com>
2020-11-30 23:25:17 +0900

    define query

--- query.go
+++ query.go
@@ -1,6 +1,26 @@
 package stalefish
 
-type Query struct {
-	Keyword string
-	Fields  []string
+type Query interface {
+	Searcher() (Searcher, error)
+}
+
+type PhraseQuery struct {
+	Phrase   string
+	Analyzer Analyzer
+}
+
+func NewPhraseQuery(phrase string, analyzer Analyzer) *PhraseQuery {
+	return &PhraseQuery{
+		Phrase:   phrase,
+		Analyzer: analyzer,
+	}
+}
+
+func (q *PhraseQuery) Searcher() (Searcher, error) {
+	tokens := q.Analyzer.Analyze(q.Phrase)
+	s, err := NewPhraseSearcher(tokens)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
 }
