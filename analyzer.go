5834b7af787a868a0d49040e06aec556c3ffa5da
Kotaro Adachi <k33asby@gmail.com>
2021-04-10 01:15:04 +0900

    refactoring

--- analyzer.go
+++ analyzer.go
@@ -1,25 +1,25 @@
 package stalefish
 
 type Analyzer struct {
-	charFilters  []CharFilter
-	tokenizer    Tokenizer
-	tokenFilters []TokenFilter
+	CharFilters  []CharFilter
+	Tokenizer    Tokenizer
+	TokenFilters []TokenFilter
 }
 
-func NewAnalyzer(charFilters []CharFilter, tokenizer Tokenizer, tokenFilters []TokenFilter) Analyzer {
-	return Analyzer{
-		charFilters:  charFilters,
-		tokenizer:    tokenizer,
-		tokenFilters: tokenFilters,
+func NewAnalyzer(charFilters []CharFilter, tokenizer Tokenizer, tokenFilters []TokenFilter) *Analyzer {
+	return &Analyzer{
+		CharFilters:  charFilters,
+		Tokenizer:    tokenizer,
+		TokenFilters: tokenFilters,
 	}
 }
 
-func (a Analyzer) Analyze(s string) *TokenStream {
-	for _, c := range a.charFilters {
+func (a *Analyzer) Analyze(s string) *TokenStream {
+	for _, c := range a.CharFilters {
 		s = c.Filter(s)
 	}
-	tokenStream := a.tokenizer.Tokenize(s)
-	for _, f := range a.tokenFilters {
+	tokenStream := a.Tokenizer.Tokenize(s)
+	for _, f := range a.TokenFilters {
 		tokenStream = f.Filter(tokenStream)
 	}
 	return tokenStream
