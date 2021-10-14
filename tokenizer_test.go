3d2ac73551afe902e156f27495887be1d02792dc
Kotaro Adachi <k33asby@gmail.com>
2021-10-15 01:33:01 +0900

    implement ngram tokenizer (#21)

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -2,6 +2,7 @@ package stalefish
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
@@ -49,6 +50,60 @@ func TestMorphologicalTokenizerTokenize(t *testing.T) {
 			// Then
 			if diff := cmp.Diff(actual, tt.expected); diff != "" {
 				t.Errorf("Diff: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestNgramTokenizer_Tokenize(t *testing.T) {
+	tests := []struct {
+		n        int
+		text     string
+		expected *TokenStream
+	}{
+		{
+			n:        1,
+			text:     "hogefuga",
+			expected: &TokenStream{Tokens: []Token{{Term: "h"}, {Term: "o"}, {Term: "g"}, {Term: "e"}, {Term: "f"}, {Term: "u"}, {Term: "g"}, {Term: "a"}}},
+		},
+		{
+			n:        2,
+			text:     "hogefuga",
+			expected: &TokenStream{Tokens: []Token{{Term: "ho"}, {Term: "og"}, {Term: "ge"}, {Term: "ef"}, {Term: "fu"}, {Term: "ug"}, {Term: "ga"}}},
+		},
+		{
+			n:        3,
+			text:     "hogefuga",
+			expected: &TokenStream{Tokens: []Token{{Term: "hog"}, {Term: "oge"}, {Term: "gef"}, {Term: "efu"}, {Term: "fug"}, {Term: "uga"}}},
+		},
+		{
+			n:        1,
+			text:     "日本昔ばなし",
+			expected: &TokenStream{Tokens: []Token{{Term: "日"}, {Term: "本"}, {Term: "昔"}, {Term: "ば"}, {Term: "な"}, {Term: "し"}}},
+		},
+		{
+			n:        2,
+			text:     "日本昔ばなし",
+			expected: &TokenStream{Tokens: []Token{{Term: "日本"}, {Term: "本昔"}, {Term: "昔ば"}, {Term: "ばな"}, {Term: "なし"}}},
+		},
+		{
+			n:        6,
+			text:     "日本昔ばなし",
+			expected: &TokenStream{Tokens: []Token{{Term: "日本昔ばなし"}}},
+		},
+		{
+			n:        7,
+			text:     "日本昔ばなし",
+			expected: &TokenStream{Tokens: []Token{}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("n = %v, text = %v, expected = %v", tt.n, tt.text, tt.expected), func(t *testing.T) {
+			tr := &NgramTokenizer{
+				n: tt.n,
+			}
+			if got := tr.Tokenize(tt.text); !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("NgramTokenizer.Tokenize() = %v, expected %v", got, tt.expected)
 			}
 		})
 	}
